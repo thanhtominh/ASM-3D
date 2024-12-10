5d2883b1e0f0f5959a11d9d17e803734d7f9b984
thanhtominh <91857070+thanhtominh@users.noreply.github.com>
2024-12-11 02:07:41 +0700

    .

--- ASM3D/Assets/Thanh`/script/DamageZone.cs
+++ ASM3D/Assets/Thanh`/script/DamageZone.cs
@@ -10,6 +10,7 @@ public class DamageZone : MonoBehaviour
     public string targetTag;
     public List<Collider> colliderTargets = new List<Collider>();
 
+    public bool flag = false;
     private void Start()
     {
         damageCollider.enabled = false;
@@ -17,25 +18,32 @@ public class DamageZone : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag(targetTag) && !colliderTargets.Contains(other))
+        Debug.Log($"**********21");
+        if (other.gameObject.CompareTag(targetTag) && !colliderTargets.Contains(other) && !flag)
         {
+            Debug.Log($"*********24");
             colliderTargets.Add(other);
             var go = other.GetComponent<Health>();
             if (go != null)
             {
+                Debug.Log($"**********29");
                 go.TakeDamage(damageAmount);
             }
+            flag = true;    
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log($"*********38");
         if (other.gameObject.CompareTag(targetTag) && !colliderTargets.Contains(other))
         {
+            Debug.Log($"*********41");
             colliderTargets.Add(other);
             var go = other.GetComponent<Health>();
             if (go != null)
             {
+                Debug.Log($"**********46");
                 go.TakeDamage(damageAmount);
             }
         }
@@ -43,12 +51,16 @@ public class DamageZone : MonoBehaviour
 
     public void BeginAttack()
     {
+        Debug.Log($"**********54");
+        flag = true;
         colliderTargets.Clear();
         damageCollider.enabled = true;
     }
 
     public void EndAttack()
     {
+        Debug.Log($"*********62");
+        flag &= false;
         colliderTargets.Clear();
         damageCollider.enabled = false;
     }
